993274cec922b40fca07ec983d005c183331e975
abithafelicia <abithafeliciya@gmail.com>
2025-11-10 00:16:04 +0530

    Add root route to fix Cannot GET /

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => res.send('Marketplace API is running'));
 app.use('/api/auth', authRoutes);
 
 // Connect to MongoDB
