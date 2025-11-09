98c72ea733957d0ee9cc81c9d4a8247af86ccba6
URK23CS1227 <167565629+AbithaFelicia@users.noreply.github.com>
2025-11-09 17:40:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import AddProduct from './components/AddProduct';
+import ViewProducts from './components/ViewProducts';
+import ManageProducts from './components/ManageProducts';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-product" element={<AddProduct />} />
+          <Route path="/view-products" element={<ViewProducts />} />
+          <Route path="/manage-products" element={<ManageProducts />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
